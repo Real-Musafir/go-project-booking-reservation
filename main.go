960bfc3f69f9d9459e75f253b07fb934a0b79062
alshahadath <abdullah.alshahadath@vstinc.com>
2024-09-23 19:32:36 +0600

    added about and home page handler

--- main.go
+++ main.go
@@ -6,14 +6,26 @@ import (
 	"net/http"
 )
 
+// Home page handler
+func Home (w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "This is the home page")
+}
+
+// Anout page handler
+func About (w http.ResponseWriter, r *http.Request){
+	sum := addValues(2,2)
+	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is tthe about page and 2+2 is %d", sum))
+}
+
+func addValues(x, y int) int {
+	sum := x+y
+	return sum
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "Hello world")
-		if err != nil {
-			fmt.Println(err)
-		}
-		fmt.Println(fmt.Sprintf("Number of bytes written: %d", n))
-	})
+
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
 	println("Starting server on :8080")
 
