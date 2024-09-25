e5d26e23fd8fe5695979512e37d71ab94962cb8b
alshahadath <abdullah.alshahadath@vstinc.com>
2024-09-25 10:50:48 +0600

    seperate render page

--- handlers.go
+++ handlers.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
@@ -16,11 +14,3 @@ func About (w http.ResponseWriter, r *http.Request){
 	renderTemplate(w, "about.page.html")
 }
 
-func renderTemplate(w http.ResponseWriter, tmpl string){
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w,nil)
-
-	if err != nil {
-		fmt.Println("error parsing template:", err)
-	}
-}
