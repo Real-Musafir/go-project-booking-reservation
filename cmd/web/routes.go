148899f4c23217516fb9bae045585345afde6446
alshahadath <abdullah.alshahadath@vstinc.com>
2024-09-29 11:20:26 +0600

    brought the middleware

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -7,11 +7,15 @@ import (
 	"github.com/alshahadath/go-web/pkg/handlers"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func routes(app *config.AppConfig) http.Handler {
 
 	mux := chi.NewRouter()
+
+	// Recoverer is a middleware that recovers from panics, logs the panic
+	mux.Use(middleware.Recoverer)
 
 	mux.Get("/", handlers.Repo.Home)
 	mux.Get("/about", handlers.Repo.About)
