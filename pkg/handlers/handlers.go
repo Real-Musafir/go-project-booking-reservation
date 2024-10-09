6949642a763174f87b9c6f269e6a1e6bf43d8219
alshahadath <abdullah.alshahadath@vstinc.com>
2024-10-09 21:39:30 +0600

    build search-availability-json end point

--- pkg/handlers/handlers.go
+++ pkg/handlers/handlers.go
@@ -1,7 +1,9 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/Real-Musafir/go-project-booking-reservation/pkg/config"
@@ -78,6 +80,27 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("Start date is %s and end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK 		bool `json:"ok"`
+	Message string `json:"message"`
+}
+// AvailabilityJSON hanles request for a availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse {
+		OK: true,
+		Message: "Available",
+	}
+
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err!=nil {
+		log.Println(err)
+	}
+
+	log.Println(string(out))
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Conact renders the contact page
