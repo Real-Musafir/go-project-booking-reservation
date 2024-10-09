dd71b71f48e83eddd477bd75a6be129e891ab15a
alshahadath <abdullah.alshahadath@vstinc.com>
2024-10-09 09:30:38 +0600

    complete the post form is search-availability

--- pkg/render/render.go
+++ pkg/render/render.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/Real-Musafir/go-project-booking-reservation/pkg/config"
 	"github.com/Real-Musafir/go-project-booking-reservation/pkg/models"
+	"github.com/justinas/nosurf"
 )
 
 
@@ -23,12 +24,13 @@ func NewTemplate(a *config.AppConfig){
 	app = a
 }
 
-func AddDefaultData(td *models.TemplateData) *models.TemplateData{
+func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData{
+	td.CSRFToken = nosurf.Token(r)
 	return td
 }
 
 //RenderTemplate renders templates using html/temp
-func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData){
+func RenderTemplate(w http.ResponseWriter,r *http.Request, tmpl string, td *models.TemplateData){
 	var tc map[string]*template.Template
 	if app.UseCache {
 		tc = app.TemplateCache
@@ -46,7 +48,7 @@ func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData)
 
 	buf := new(bytes.Buffer)
 
-	td = AddDefaultData(td)
+	td = AddDefaultData(td, r)
 
 	_= t.Execute(buf, td)
 
